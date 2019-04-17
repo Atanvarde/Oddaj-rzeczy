c7d3b38ff2fa6311dc1088a0d32944aa85efaa6c
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-04-17 17:22:36 +0200

    Section_yellow_layout

--- src/Components/Landing_page/YellowSection/YellowSection.jsx
+++ src/Components/Landing_page/YellowSection/YellowSection.jsx
@@ -8,17 +8,17 @@ class YellowSection extends React.Component {
                 <div className="Yellow_ctn">
                     <h1 className="Yellow_ctn_quantity">10</h1>
                     <h2 className="Yellow_ctn_header">ODDANYCH WORKÓW</h2>
-                    <p className="Yellow_ctn_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos est hic illum laboriosam magnam provident recusandae reiciendis rem, reprehenderit! Iste labore porro quos sit.</p>
+                    <p className="Yellow_ctn_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos est hic illum laboriosam magnam provident recusandae reiciendis rem, reprehenderit!</p>
                 </div>
                 <div className="Yellow_ctn">
                     <h1 className="Yellow_ctn_quantity">5</h1>
                     <h2 className="Yellow_ctn_header">WSPARTYCH ORGANIZACJI</h2>
-                    <p className="Yellow_ctn_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos est hic illum laboriosam magnam provident recusandae reiciendis rem, reprehenderit! Iste labore porro quos sit.</p>
+                    <p className="Yellow_ctn_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos est hic illum laboriosam magnam provident recusandae reiciendis rem, reprehenderit!</p>
                 </div>
                 <div className="Yellow_ctn">
                     <h1 className="Yellow_ctn_quantity">7</h1>
                     <h2 className="Yellow_ctn_header">ZORGANIZOWANYCH ZBIÓREK</h2>
-                    <p className="Yellow_ctn_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos est hic illum laboriosam magnam provident recusandae reiciendis rem, reprehenderit! Iste labore porro quos sit.</p>
+                    <p className="Yellow_ctn_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos est hic illum laboriosam magnam provident recusandae reiciendis rem, reprehenderit!</p>
                 </div>
             </section>
         )
