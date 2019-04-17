d3ed7ab407e82889366d6b51687d8a8e749e3cf2
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-04-17 12:33:55 +0200

    Yellow section added

--- src/Components/Landing_page/LandingPage.jsx
+++ src/Components/Landing_page/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './LandingPage.scss';
 import Header from './Header/Header.jsx';
 import Navigation from './Navigation/Navigation.jsx';
+import YellowSection from "./YellowSection/YellowSection";
 
 class LandingPage extends React.Component {
     render() {
@@ -9,6 +10,7 @@ class LandingPage extends React.Component {
             <div className="LandingPage_container">
                 <Navigation/>
                 <Header/>
+                <YellowSection/>
             </div>
         )
     }
---
+++ src/Components/Landing_page/YellowSection/YellowSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './YellowSection.scss';
+
+class YellowSection extends React.Component {
+    render() {
+        return (
+            <section className="Statistic_container">
+                <div className="Yellow_ctn">
+                    <h1 className="Yellow_ctn_quantity">10</h1>
+                    <h2 className="Yellow_ctn_header">ODDANYCH WORKÓW</h2>
+                    <p className="Yellow_ctn_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos est hic illum laboriosam magnam provident recusandae reiciendis rem, reprehenderit! Iste labore porro quos sit.</p>
+                </div>
+                <div className="Yellow_ctn">
+                    <h1 className="Yellow_ctn_quantity">5</h1>
+                    <h2 className="Yellow_ctn_header">WSPARTYCH ORGANIZACJI</h2>
+                    <p className="Yellow_ctn_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos est hic illum laboriosam magnam provident recusandae reiciendis rem, reprehenderit! Iste labore porro quos sit.</p>
+                </div>
+                <div className="Yellow_ctn">
+                    <h1 className="Yellow_ctn_quantity">7</h1>
+                    <h2 className="Yellow_ctn_header">ZORGANIZOWANYCH ZBIÓREK</h2>
+                    <p className="Yellow_ctn_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim eos est hic illum laboriosam magnam provident recusandae reiciendis rem, reprehenderit! Iste labore porro quos sit.</p>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default YellowSection;
