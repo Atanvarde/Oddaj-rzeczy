c231ac9ffb728a6b00fa676aba64292706632f9f
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-05-16 15:21:28 +0200

    Contact form

--- src/Components/Landing_page/ContactSection/ContactForm/ContactForm.jsx
+++ src/Components/Landing_page/ContactSection/ContactForm/ContactForm.jsx
@@ -2,10 +2,58 @@ import React from 'react';
 import './ContactForm.scss';
 
 class ContactForm extends React.Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            name:'',
+            email:'',
+            message:''
+        };
+    }
+
+    handleNameChange = (event) => {
+        this.setState( {
+            name: event.target.value
+        });
+    };
+
+    handleEmailChange = (event) => {
+        this.setState({
+            email: event.target.value
+        });
+    };
+
+    handleMessageChange = (event) => {
+        this.setState({
+            message: event.target.value
+        });
+    };
+
+
     render() {
         return (
-            <div>
-
+            <div className="container">
+                <h1 className="form__header">FORMULARZ KONTAKTOWY</h1>
+                <form className="container__contactForm">
+                    <label>
+                        Imię
+                        <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
+                        <hr></hr>
+                    </label>
+                    <label>
+                        Email
+                        <input type="email" value={this.state.email} onChange={this.handleEmailChange} />
+                        <hr></hr>
+                    </label>
+                    <label>
+                        Wiadomość
+                        <input type="text" value={this.state.message} onChange={this.handleMessageChange}/>
+                        <hr></hr>
+                    </label>
+                    <div className="button_submit__container">
+                        <input className="button__submit" type="submit" value="Wyślij" />
+                    </div>
+                </form>
             </div>
         );
     }
--- src/Components/Landing_page/ContactSection/ContactSection.jsx
+++ src/Components/Landing_page/ContactSection/ContactSection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './ContactSection.scss';
 import decoration from '../../../images/decoration.png';
 import ContactForm from './ContactForm/ContactForm.jsx';
+import contactIcons from '../../../images/icons_contactsection.png';
 
 class ContactSection extends React.Component {
     render() {
@@ -12,8 +13,11 @@ class ContactSection extends React.Component {
                     <figure>
                         <img src={decoration} alt="decoration"></img>
                     </figure>
+                    <ContactForm/>
                 </div>
-                <ContactForm/>
+                <figure>
+                    <img className="contact-icons" src={contactIcons} alt = "facebook and telephone contact icons"></img>
+                </figure>
                 <footer>
                     <p>Copyright 2015 &copy; by Anna Dadej</p>
                 </footer>
