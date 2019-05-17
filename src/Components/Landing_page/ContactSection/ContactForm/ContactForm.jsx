8ab42ac3a5007dd7c332163fc6ce589fb662f9d6
Aleksandra Gardo <aleksandra.gardo@gmail.com>
2019-05-17 13:43:24 +0200

    contact form css

--- src/Components/Landing_page/ContactSection/ContactForm/ContactForm.jsx
+++ src/Components/Landing_page/ContactSection/ContactForm/ContactForm.jsx
@@ -34,24 +34,26 @@ class ContactForm extends React.Component {
         return (
             <div className="container">
                 <h1 className="form__header">FORMULARZ KONTAKTOWY</h1>
-                <form className="container__contactForm">
-                    <label>
-                        Imię
-                        <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
-                        <hr></hr>
-                    </label>
-                    <label>
-                        Email
-                        <input type="email" value={this.state.email} onChange={this.handleEmailChange} />
-                        <hr></hr>
-                    </label>
-                    <label>
-                        Wiadomość
-                        <input type="text" value={this.state.message} onChange={this.handleMessageChange}/>
-                        <hr></hr>
-                    </label>
+                <form className="form__container">
+                    <div className="form__container--label">
+                        <label>
+                            Imię
+                            <input className="contact__input--name" type="text" value={this.state.name} onChange={this.handleNameChange}/>
+                            <hr></hr>
+                        </label>
+                        <label>
+                            Email
+                            <input className="contact__input--email" type="email" value={this.state.email} onChange={this.handleEmailChange} />
+                            <hr></hr>
+                        </label>
+                        <label>
+                            Wiadomość
+                            <input className="contact__input--message" type="text" value={this.state.message} onChange={this.handleMessageChange}/>
+                            <hr></hr>
+                        </label>
+                    </div>
                     <div className="button_submit__container">
-                        <input className="button__submit" type="submit" value="Wyślij" />
+                        <button className="contact__button" type="submit">Wyślij</button>
                     </div>
                 </form>
             </div>
